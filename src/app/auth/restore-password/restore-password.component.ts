58c3e2cd283b5fd002b676734456d05c9e806708
Paul_Luque <pluquec@unsa.edu.pe>
2024-05-27 20:06:47 -0500

    cambiar entre texto y password

--- src/app/auth/restore-password/restore-password.component.ts
+++ src/app/auth/restore-password/restore-password.component.ts
@@ -10,11 +10,25 @@ import { Component } from '@angular/core';
 export class RestorePasswordComponent  {
 
   showFirstPart: boolean = true;
+  showPassword1: boolean = false;
+  showPassword2: boolean = false;
+  showPassword3: boolean = false;
+
+
 
   continue() {
     this.showFirstPart = false;
   }
+  togglePassword1(){
+    this.showPassword1 = !this.showPassword1;
+  }
 
+  togglePassword2(){
+    this.showPassword2 = !this.showPassword2;
+  }
+  togglePassword3(){
+    this.showPassword3 = !this.showPassword3;
+  }
 
 
 }
