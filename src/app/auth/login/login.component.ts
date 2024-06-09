7347948376c561782de8a22752f26b9791dac56f
Paul_Luque <pluquec@unsa.edu.pe>
2024-06-09 09:19:15 -0500

    Use service in login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { AuthService } from '../services/auth.service';  // Asegúrate de ajustar la ruta según tu estructura de proyecto
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  constructor(private authService: AuthService) { }
 
+  onLogin(): void {
+    if(this.username == "" || this.password == ""){
+      alert("Ingrese usuario y contraseña")
+    }
+    this.authService.login(this.username, this.password);
+  }
+
+  onMicrosoftLogin(): void {
+    this.authService.loginWithMicrosoft('Has accedido con Microsoft.');
+  }
 }
